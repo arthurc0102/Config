99a0893e7be49f4d02e32ca9806fc9c2c7d42e3d
Arthur Chang <arthurc0102@gmail.com>
2021-07-09 12:50:02 +0800

    Change zplug

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,32 +1,26 @@
-## Load antigen
-source "$HOME/.antigen/bin/antigen.zsh"
+# Load zplug.
+source "${HOME}/.zplug/init.zsh"
 
 ## ssh-agent config should place before loading oh-my-zsh.
 [ -z "$SHORT_HOST" ] && export SHORT_HOST="localhost"
 zstyle :omz:plugins:ssh-agent identities $SSH_AGENT_IDENTITIES
 
-## Load the oh-my-zsh's library.
-antigen use oh-my-zsh
+# Load plugin.
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/ssh-agent", from:oh-my-zsh
+zplug "plugins/pip", from:oh-my-zsh
+zplug "plugins/docker", from:oh-my-zsh
+zplug "plugins/docker-compose", from:oh-my-zsh
+zplug "plugins/web-search", from:oh-my-zsh
+zplug "zpm-zsh/clipboard"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "qoomon/zsh-lazyload"
+zplug "zsh-users/zsh-completions", defer:2
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
-## Load plugins
-antigen bundle git
-antigen bundle ssh-agent
-antigen bundle pip
-antigen bundle docker
-antigen bundle docker-compose
-antigen bundle web-search
-antigen bundle qoomon/zsh-lazyload
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-autosuggestions
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zpm-zsh/clipboard
-
-## Setup theme
+# Theme config.
 source "$HOME/.dotfiles/zsh/theme.zsh"
-antigen theme denysdovhan/spaceship-prompt
-
-## Apply settings
-antigen apply
+zplug "spaceship-prompt/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
 
 ## Load custom settings
 source "$HOME/.dotfiles/zsh/alias.zsh"
@@ -37,6 +31,18 @@ source "$HOME/.dotfiles/zsh/environments/golang.zsh"
 source "$HOME/.dotfiles/zsh/environments/mysql.zsh"
 source "$HOME/.dotfiles/zsh/environments/node.zsh"
 source "$HOME/.dotfiles/zsh/environments/python.zsh"
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load
+# zplug load --verbose
 
 ## Lazyload
 lazyload pyenv python pip pipx -- 'load_pyenv'
